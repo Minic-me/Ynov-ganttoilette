33a42454c9b00820a5b4441cfb15937ccb15a641
Juliette Mai <juliette.mai@ynov.com>
2017-04-06 16:07:59 +0000

    Add functions

--- server/Controller/projectListController.js
+++ server/Controller/projectListController.js
@@ -5,9 +5,42 @@ const router = express.Router();
 
 console.log("OK");
 
-router.get('/createProject', (req, res) => {
+// Variables
+var liste = [];
+var archive = [];
+
+// Add a project
+router.post('/createProject', (req, res) => {
     console.log(req.query);
     console.log('Adding project...');
+
+    let oneProject = req.body.project;
+
+    liste.push(oneProject);
+    archive.push({
+        action: 'add',
+        status: 'success',
+        ip: req.headers['x-forwarded-for'],
+        date: Date()
+    });
+
+    console.log("\nAdding a project" + archive[archive.length - 1]);
+
+    res.end();
+});
+
+
+// Delete a project
+router.delete('/deleteProject/:id', (req, res, next) => {
+    liste.splice(req.params.id, 1);
+    res.end();
+
+});
+
+router.get('/getProjects', (req, res) => {
+    res.status(200);
+    res.json(liste);
+    console.log("test server getprojects");
 });
 
 exports.router = router;
