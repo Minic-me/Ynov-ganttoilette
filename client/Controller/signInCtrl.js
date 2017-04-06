fbea89d9645948f1717711697179c8ff5059afff
cemkarahan84 <>
2017-04-06 14:05:39 +0000

    First commit

---
+++ client/Controller/signInCtrl.js
@@ -0,0 +1,36 @@
+app.controller('signInCtrl', ['$scope', '$http', function($scope, $http) {
+
+    const serverName = "https://gantt-kagomuffex.c9users.io"
+
+    $scope.SignIn = function() {
+        var sign = $scope.sign;
+        let user = {
+            email: sign.email,
+            password: sign.password
+        };
+
+        $http.get(serverName + "user/signIn", {
+                params: user
+            })
+            .success(function(user) {
+
+
+            });
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}]);
